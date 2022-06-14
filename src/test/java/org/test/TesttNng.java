29530e9c8500ff1f8125e4a1002b10f8baaf5b7a
Pattatharasan <pattatharasan@gmail.com>
2022-06-14 22:52:31 +0530

    project model

---
+++ src/test/java/org/test/TesttNng.java
@@ -0,0 +1,19 @@
+package org.test;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TesttNng {
+	
+	@BeforeClass
+	private void beforeClass() {
+		
+
+	}
+
+	@Test
+	private void tesy() {
+		
+
+	}
+}
